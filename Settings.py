85d256e8f9a96971b96eae374e883a93771c299f
Arinze Okeke <>
2018-10-06 11:14:08 -0400

    study and added a time block between 5 days and 25 days and between 25 days and 4 months and between 4 months and 2 years

--- Settings.py
+++ Settings.py
@@ -7,8 +7,8 @@ Created on Sat Sep  1 20:12:20 2018
 """
 
 maxStudyGroup=2
-# 5 seconds, 25 seconds, 2 minutes, 10 minutes, 1 hour, 5 hours, 1 day, 5 days, 25 days, 4 months, and 2 years
-timeToNext = (5,25,2*60,10*60,1*60*60,5*60*60,1*24*60*60,5*24*60*60,25*24*60*60,4*30*24*60*60,2*365*24*60*60)
+#             5 seconds, 25 seconds, 2 minutes, 10 minutes, 1 hour, 5 hours, 1 day,     5 days,    10 days,    25 days,    2 months,     4 months,     1 years,   and 2 years
+timeToNext = (5,        25,         2*60,       10*60,      1*60*60,5*60*60, 1*24*60*60,5*24*60*60,10*24*60*60,25*24*60*60,2*30*24*60*60,4*30*24*60*60,1*365*24*60*60,2*365*24*60*60)
 yes = ["y","yes","1"]
 no = ["n","no","0"]
 stop = ["end","done","finish","finished","close","exit","quit"]
