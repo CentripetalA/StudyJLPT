2e9f3c65d08580f330286c3beff85b9fea4fc3df
Arinze's RYZEN Laptop <arinzeokeke0@gmail.com>
2018-11-01 05:30:10 -0400

    study

--- Settings.py
+++ Settings.py
@@ -19,7 +19,7 @@ fullKanji="Kanji.tsv"
 trimKanji="KanjiTrim.tsv"
 wordList = {"vocab":trimVocab,"kanji":trimKanji}
 
-studySet = {"vocab":range(91),"kanji":range(30),"kanji2":range(70)}
+studySet = {"vocab":range(91),"kanji":range(30),"kanji2":range(72)}
 
 saveFileNames = {"vocab":"savedVocabDeck.json","kanji":"savedKanjiDeck.json","kanji2":"savedKanji2Deck.json"}
 
